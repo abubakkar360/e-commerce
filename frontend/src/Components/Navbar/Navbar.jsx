cc29dcafdc7cf27787178c0335841508293b45af
abubakkar360 <91024041+abubakkar360@users.noreply.github.com>
2026-08-17 11:06:52 +0600

    making responsive using css

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,21 +2,29 @@
 import "./Navbar.css"
 import logo from '../Assets/logo.webp'
 import { FaCartPlus } from "react-icons/fa";
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../assets/Context/ShopContextInstance";
-
+import { IoIosArrowDropdown } from "react-icons/io";
 
 const Navbar = () => {
   const {getTotalCartItems} = useContext(ShopContext);
   const [menu,setMenu] = useState("shop")
+  const menuRef = useRef();
+
+  const dropdownToggle = (e)=>{
+    menuRef.current.classList.toggle('nav_menu_visible');
+    e.target.classList.toggle('open')
+  }
+
   return (
     <div>
       <div className="navber">
         <div className="nav_logo">
           <Link to={'/'}><img src={logo} alt="" /></Link>
         </div>
-        <ul className="nav_menu">
+        < IoIosArrowDropdown className="nav_dropdown" onClick={dropdownToggle}/>
+        <ul ref={menuRef} className="nav_menu">
           <li onClick={()=>{setMenu("shop")}}> <Link to={'/'} className="link">Shop</Link>  {menu==="shop"?<hr/>:<></>}</li>
           <li onClick={()=>{setMenu("mens")}}> <Link to={'/mens'} className="link">Men</Link>{menu==="mens"?<hr/>:<></>}</li>
           <li onClick={()=>{setMenu("womens")}}> <Link to={'/womens'} className="link">Women</Link>{menu==="womens"?<hr/>:<></>}</li>
--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -7,10 +7,13 @@ const NewCollections = () => {
       <h1>New collections</h1>
       <hr />
       <div className="collections">
-        {new_collection.map((item,i)=>{
-                        return <Item key={i} id={item.id} name = {item.name} image={item.image} new_price = {item.new_price} old_price = {item.old_price}/>
+        <div className="collections_wrapper">
+          {new_collection.map((item,i)=>{
+                        return <Item className={item} key={i} id={item.id} name = {item.name} image={item.image} new_price = {item.new_price} old_price = {item.old_price}/>
 
         })}
+        </div>
+        
       </div>
     </div>
   )
